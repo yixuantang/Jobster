d5828dcabd7e73bb61464a1f495abf50aa104803
Ben Steers <ben@bensteers.me>
2018-05-16 19:49:11 -0400

    fixed student update

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ from flask_login import LoginManager, UserMixin, login_required, login_user, log
 
 from forms import *
 from auth import User
-
-from functions import (_listfollowers_company, _jobs, _student, updateprofile, _postjobs, hash_password, _listfriends, 
-                    _student_from_id, _com,_listfollowers_user, _com_from_id,_studentregister,_companyregister, 
-                    _veristudentpassword, _vericompanypassword, _Notifications, _selectjob)
+from functions import *
 
 
 
@@ -41,11 +38,16 @@ login_manager.user_loader(lambda id: User.from_id(id, type='student' if id.start
 
 @app.context_processor
 def add_global_template_vars():
-    notifications = _Notifications(current_user.id) if not current_user.is_anonymous else None
+    notifications = add_notification(current_user.id) if not current_user.is_anonymous else None
     return dict(
         search_form=SearchForm(query=request.args.get('query')), # add search form for every page
         notifications=notifications
     )
+
+
+def student_login_required(func):
+    pass
+
 
 '''
 
@@ -67,38 +69,30 @@ def search():
 
 @app.route('/student/<user>')
 def student(user):
-    student_data = _student(user) # get info for a student
-    following = _listfollowers_user(user)
-    friends = _listfriends(user)
+    student_data =  stud(user) # get info for a student
+    following = listfollowers_user(user)
+    friends = listfriends(user)
     return render_template('pages/student.j2', user=student_data, following=following, friends=friends)
-
-@app.route('/student/<user>/edit')
-@login_required
-def student_edit(user):
-    student_data = _student(user) # get info for a student
-    following = _listfollowers_user(user)
-    friends = _listfriends(user)
-    return render_template('pages/update_profile.j2', user=student_data, following=following, friends=friends)
 
 
 @app.route('/company/<user>')
 def company(user):
-    company_data =_com(user)
-    followers = _listfollowers_company(user)
-    jobs = _jobs(user)
+    company_data = com(user)
+    followers = listfollowers_company(user)
+    jobs = jobs(user)
     return render_template('pages/company.j2', company=company_data, followers=followers, jobs=jobs)
 
 
 @app.route('/job/<aid>')
 def job(aid=None):
-    thejob_data = _selectjob(aid)
+    thejob_data = selectjob(aid)
     return render_template('pages/job.j2', job=thejob_data, aid=aid)
 
 
 @app.route('/notifications')
 @login_required
 def notifications():
-    Noti_data = _Notifications(current_user.id)
+    Noti_data = add_notifications(current_user.id)
     return render_template('pages/notification.j2', notifs=Noti_data, user=current_user)
 
 
@@ -114,24 +108,24 @@ def notifications():
 
 # @app.route('/following/<user>')
 # def following(user):
-#     company_data = _listfollowers_user(user)
+#     company_data = listfollowers_user(user)
 #     return render_template('pages/following.j2', user=company_data)
 
 
 # @app.route('/friends/<user>')
 # def friends(user):
-#     friends_data = _listfriends(user)
+#     friends_data = listfriends(user)
 #     return render_template('pages/friends.j2', user=friends_data)
 
 
 # @app.route('/follower/<company>')
 # def followers(company):
-#     company_data = _listfollowers_company(company)
+#     company_data = listfollowers_company(company)
 #     return render_template('pages/follower.j2', user=company_data)
 
 # @app.route('/jobs/<company>')
 # def jobs(company=None):
-#     jobs_data = _jobs(company)
+#     jobs_data = jobs(company)
 #     return render_template('pages/job.j2', jobs=jobs_data, company=company)
 
 
@@ -147,27 +141,34 @@ Asynchronous Methods
 @app.route('/notification/<aid>/<sid>/mark_read', methods=['POST'])
 @login_required
 def mark_read(aid, sid):
-    # _mark_as_read(aid, sid) # mark notification as read
+    status = read_notification(aid, sid) # mark notification as read
     return jsonify({
-        'success': 1
+        'success': status
     })
 
-@app.route('/student/<user>/follow', methods=['POST'])
+@app.route('/company/<user>/follow', methods=['POST'])
 @login_required
-def follow_student(user):
-    # _follow_student(user) 
+def follow(user):
+    status = follow_com(user, current_user.id) 
     return jsonify({
-        'success': 1
+        'success': status
     })
 
 @app.route('/student/<user>/befriend', methods=['POST'])
 @login_required
 def friend_request(user):
-    # _send_friend_request(user)
+    status = send_friend_request(current_user.id, user)
     return jsonify({
-        'success': 1
+        'success': status
     })
 
+@app.route('/apply/<aid>/<contact_by>', methods=['POST'])
+@login_required
+def apply(aid, contact_by):
+    status = sendapplication(aid, current_user.sid, contact_by)
+    return jsonify({
+        'success': status
+    })
 
 
 '''
@@ -201,13 +202,13 @@ def register():
     if request.method == 'POST' and form.validate():
         user = User.from_form(form)
         if form.act_type.data == 'company':
-            _companyregister(form.name.data, form.password.data, form.username.data)
+            companyregister(form.name.data, form.password.data, form.username.data)
             if user:
                 login_user(user)
                 print('logged in company', user.username)
                 return redirect(url_for('company', user=user.username))
         elif form.act_type.data == 'student':
-            _studentregister(form.name.data, form.password.data, form.username.data)
+            studentregister(form.name.data, form.password.data, form.username.data)
             # user = User.from_form(form)
             if user:
                 login_user(user)
@@ -217,35 +218,35 @@ def register():
 
 
 
-@app.route('/apply/<aid>', methods=['GET', 'POST'])
-@login_required
-def apply(aid):
-    form = ApplicationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        pass #_sendapplication(form.email_phone, current_user,now())
-    return render_template('form/apply.j2', form=form)
+# @app.route('/apply/<aid>', methods=['GET', 'POST'])
+# @login_required
+# def apply(aid):
+#     form = ApplicationForm(request.form)
+#     if request.method == 'POST' and form.validate():
+#         pass #_sendapplication(form.email_phone, current_user,now())
+#     return render_template('form/apply.j2', form=form)
 
 
 @app.route('/student/<user>/update', methods=['GET', 'POST'])
 @login_required
-def update_profile(user):
-    form = UpdateForm(request.form)
-    student_data = _student(user) # get info for a student
+def student_update(user):
+    student_data = stud(user) # get info for a student
+    form = UpdateForm(**student_data)
     if request.method == 'POST' and form.validate():
         print(current_user)
         print('122')
-        updateprofile(form.phone.data, form.email.data, current_user)
+        updateprofile(form.phone.data, form.email.data, current_user.id)
         print(form.phone.data)
         print(form.email.data)
         print('123')
-    return render_template('form/update_profile.j2', form=form, user=student_data)
+    return render_template('form/student_update.j2', form=form, user=student_data)
 
 
 @app.route('/post_job/<user>', methods=['GET', 'POST'])
 @login_required
 def post_job(user):
     form = PostJob(request.form)
-    student_data = _student(user) # get info for a student
+    student_data =  stud(user) # get info for a student
     if request.method == 'POST' and form.validate():
         pass #
     return render_template('form/job_posting.j2', form=form, user=student_data)
@@ -276,11 +277,11 @@ def not_found_error(error):
 
 # def postjob():
 #     form = PostJob(request.form)
-#     # company_data = _com_from_id(user) # get info for a student
+#     # company_data = com_from_id(user) # get info for a student
 #     if request.method == 'POST' and form.validate():
 #         print(current_user)
 #         print('122')
-#         _postjobs(form.phone.data, form.email.data, current_user)
+#         postjobs(form.phone.data, form.email.data, current_user)
 #         print(form.phone.data)
 #         print(form.email.data)
 #         print('123')
